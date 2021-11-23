d05490737a0cba692c0076812ddb6319d5b1e15e
Shuwen Sun <sun.shuw@northeastern.edu>
2021-11-23 13:27:18 -0500

    foo

--- vm-setup.sh
+++ vm-setup.sh
@@ -11,7 +11,7 @@ sudo apt-get -q install -y cmake mg make git linux-headers-$(uname -r) clang-for
                            libcap-dev libgcrypt11-dev libglib2.0-dev \
                            qt5-default libqt5multimedia5 qtmultimedia5-dev \
                            libpcap-dev libqt5svg5-dev qttools5-dev \
-                           qttools5-dev-tools ntp
+                           qttools5-dev-tools ntp libelf-dev
 
 # Install Wireshark from src due to some vagrant/vm issues.
 # Commented out as it can take a while to setup. Use locally if you want.
